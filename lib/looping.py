c22b468e7ba3004d74249c1a1e5fbac1bb565d84
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2023-11-08 19:35:11 -0500

    complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,30 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i=10
+    while i>0:
+        print(i)
+        i-=1
+    print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    # sqaured_int_list = list()
+    # for num in int_list:
+    #     squared_num = num * num
+    #     sqaured_int_list.append(squared_num)
+    # return sqaured_int_list
+    int_list = [num* num for num in int_list]
+    return int_list
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range(1,101):
+        if i%3 == 0 and i%5 == 0:
+            print("FizzBuzz")
+        elif i%3 ==0:
+            print("Fizz")
+        elif i%5 == 0:
+            print("Buzz")
+        else:
+            print(i)
